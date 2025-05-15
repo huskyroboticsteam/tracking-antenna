056b8f767d62cadca66131eee06061a30e98dd0b
Geeoon Chung <geeoon13@gmail.com>
2025-05-14 18:23:35 -0700

    Added script

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <cmath>
 
 int main(int argc, char** argv){
-  std::cout << "Hello, world!" << std::endl;
+  // FILE* signal_strength_ptr = popen("iw wlan0 station dump | grep -E "signal:\s*" | tr -d -c "\- 0-9"");
+
   return EXIT_SUCCESS;
 }
