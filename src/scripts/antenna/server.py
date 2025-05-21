7f04e5cbae6244ac043a49bacb7da9ca5542cd42
Geeoon <geeoon13@gmail.com>
2025-05-20 17:38:33 -0700

    Basic server

---
+++ src/scripts/antenna/server.py
@@ -0,0 +1,31 @@
+# script that accepts motor commands and forwards them to the motors via UART
+
+import serial
+import socket
+
+HOST = "127.0.0.1"
+PORT = 2000  # we are using port 2000 to communicate
+MAX_MSG_SIZE = 1024  # max 1024 bytes
+
+"""
+@brief processes a message received
+@param message the message we received
+@return what we should send back in response to this message
+"""
+def process_message(message: bytes) -> bytes:
+    print(message)
+    return message
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+    sock.bind((HOST, PORT))
+    sock.listen()
+    conn, addr = sock.accept()
+    with conn:  # connection created
+        while True:
+            data = conn.recv(MAX_MSG_SIZE)
+            if not data:
+                print("Client disconnected")
+                break
+            response = process_message(data)
+            if data:
+                conn.sendall(data)  # replace with actually processing
